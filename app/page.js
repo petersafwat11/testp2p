e937c98b7b8ffc0ee15554fc3886988c2e9aa8f8
peter safwat <petersafwat640@gmail.com>
2023-12-31 18:31:09 +0200

    tring to fix

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import Head from "next/head";
 import styles from "./page.module.css";
 import VideoPlayer from "@/components/hlcPlayer/HlcPlayer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Head>
+        <script src="https://cdn.jsdelivr.net/npm/swarmcloud-hls@0.8.8/dist/hls.min.js"></script>
+      </Head>
+
       <h1>Testing</h1>
       <VideoPlayer
         streamUrl={
