09e5dcf63bf84f17238c66368e6b654e5a5709f2
peter safwat <petersafwat640@gmail.com>
2023-12-31 18:32:57 +0200

    fixed

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import styles from "./page.module.css";
 import VideoPlayer from "@/components/hlcPlayer/HlcPlayer";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Head>
-        <script src="https://cdn.jsdelivr.net/npm/swarmcloud-hls@0.8.8/dist/hls.min.js"></script>
+        <Script src="https://cdn.jsdelivr.net/npm/swarmcloud-hls@0.8.8/dist/hls.min.js"></Script>
       </Head>
 
       <h1>Testing</h1>
